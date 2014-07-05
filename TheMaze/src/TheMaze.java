8f7f772d087100fa76c0ab3af91f6df05d9e7073
sahas <sahas@Sahas-Ubuntu>
2014-07-05 12:02:17 -0700

    testing

--- TheMaze/src/TheMaze.java
+++ TheMaze/src/TheMaze.java
@@ -31,6 +31,7 @@ public class TheMaze extends GraphicsProgram {
 	private double type;
 	
 	/*when i learn how to do 2D arrays i can replace*/
+	/*Just testing git */
 	String Path = "9,9 : 9,8 : 9,7 : 8,7 : 7,7 : 6,7 : 5,7 : 4,7 : 4,6 : 4,5 : 4,4 : 5,4 : 6,4 : 7,4 : 7,3 : 7,2 : 6,2 : 5,2 : 5,1 : 5,0 ";
 	String Path1 = "9,9 : 9,8 : 9,7 : 8,7 : 7,7 : 6,7 : 6,8  : 5,8 : 4,8 : 4,7 : 4,6 : 4,5 : 5,5 : 6,5 : 7,5 : 7,4 : 7,3 : 7,2 : 6,2 : 5,2 : 4,2 : 3,2 : 3,3 : 2,3 : 1,3 : 1,2 : 1,1 : 1,0";
 	String CurrPath;
